4d4434a1ba7f610dface71ca50abb6e83a12a98f
Rayne Weinstein <98506458+RZW-13@users.noreply.github.com>
2023-04-02 22:35:59 -0500

    Update universal_topnav.js

--- js/universal_topnav.js
+++ js/universal_topnav.js
@@ -13,6 +13,48 @@ function selectTopnavUpdate(){
   window.location.href = "https://diversifying-reading.github.io/" + link.toLowerCase();
 }
 
+function resize_topnav(){
+  let options = ["Home", "Catalog", "Resources", "Suggest a Book"];
+
+  let optionsFormatted = "";
+
+  for(var i=0; i<options.length; i++){
+    optionsFormatted += "<option> ";
+    optionsFormatted += options[i];
+    optionsFormatted += "</option>"
+  }
+
+  if(window.innerWidth <= 687 && document.getElementsByClassName("topnav_text")[0].style.height == ""){
+    document.getElementsByClassName("topnav_text")[0].innerHTML = '<select class="select" onchange="selectTopnavUpdate()">' + optionsFormatted + '</select>';
+    document.getElementsByClassName("topnav_text")[0].style.height = "100%";
+    document.getElementsByClassName("topnav_text")[0].style.width = document.getElementsByClassName("select")[0].offsetWidth+10 + "px";
+    document.getElementsByClassName("topnav_text")[0].style.paddingTop = "0px";
+    document.getElementsByClassName("topnav_text")[0].style.paddingLeft = "0px";
+    document.getElementsByClassName("topnav_text")[0].style.fontSize = "";
+    document.getElementsByClassName('topnav_text')[0].style.marginLeft = "0px";
+
+    scroll_function();
+  }
+  else if(window.innerWidth > 687 && document.getElementsByClassName("select").length > 0){
+    document.getElementsByClassName("topnav_text")[0].style.height = "";
+    document.getElementsByClassName("topnav_text")[0].style.paddingLeft = "14px";
+    document.getElementsByClassName("topnav_text")[0].style.width = "";
+
+    document.getElementsByClassName("topnav_text")[0].innerHTML = '\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl" class="topnav_text_links" style="float: left; padding-top: 8px; padding-bottom: 8px;">Home</a>\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl/suggest" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Suggest a Book</a>\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl/resources" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Resources</a>\n  <a href="https://diversifying-reading.github.io/search/" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Catalog</a>\n  ';
+    document.getElementsByClassName('topnav_text')[0].style.marginLeft = "1vmin";
+
+    scroll_function();
+  }
+  if(window.innerWidth > 687){
+    document.getElementsByClassName("topnav_text")[0].style.fontSize = document.getElementById("topnav").offsetHeight/2 +"px";
+    document.getElementsByClassName("topnav_text")[0].style.fontSize = parseFloat(document.getElementsByClassName("topnav_text")[0].style.fontSize) * document.getElementById("topnav").offsetHeight;
+  }
+
+  if(document.getElementsByClassName("select").length > 0){
+    document.getElementsByClassName("select")[0].style.width = "100vw";
+  }
+}
+
 function scroll_function(){
   resize_topnav();
 
@@ -55,64 +97,9 @@ function scroll_function(){
   else{
     document.getElementById("topnav").style.height = "50px";
   }
+  document.getElementById("body_text").style.paddingTop = 54 - 49 + "px";
 
-  document.getElementById("sidenav").style.top = document.getElementById("topnav").offsetHeight + "px";
-
-}
-
-function resize_topnav(){
-  let options = ["Home", "Catalog", "Resources", "Suggest a Book"];
-  options.splice(options.indexOf(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0"), 1));
-  options.unshift(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0").charAt(0).toUpperCase() + currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0").slice(1))
-
-  let optionsFormatted = "";
-
-  for(var i=0; i<options.length; i++){
-    optionsFormatted += "<option> ";
-    optionsFormatted += options[i];
-    optionsFormatted += "</option>"
-  }
-
-  if(window.innerWidth <= 687 && document.getElementsByClassName("topnav_text")[0].style.height == ""){
-    document.getElementsByClassName("topnav_text")[0].innerHTML = '<select class="select" onchange="selectTopnavUpdate()">' + optionsFormatted + '</select>';
-    document.getElementsByClassName("topnav_text")[0].style.height = "100%";
-    document.getElementsByClassName("topnav_text")[0].style.width = document.getElementsByClassName("select")[0].offsetWidth+10 + "px";
-    document.getElementsByClassName("topnav_text")[0].style.paddingTop = "0px";
-    document.getElementsByClassName("topnav_text")[0].style.paddingLeft = "0px";
-    document.getElementsByClassName("topnav_text")[0].style.fontSize = "";
-    document.getElementById("menu_button").style.float = "right";
-    document.getElementsByClassName('topnav_text')[0].style.marginLeft = "0px";
-
-    scroll_function();
-  }
-  else if(window.innerWidth > 687 && document.getElementsByClassName("select").length > 0){
-    document.getElementsByClassName("topnav_text")[0].style.height = "";
-    document.getElementsByClassName("topnav_text")[0].style.paddingLeft = "14px";
-    document.getElementsByClassName("topnav_text")[0].style.width = "";
-
-
-    sidenav_openStatus = 2; //untouched
-
-    document.getElementsByClassName("topnav_text")[0].innerHTML = '\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl" class="topnav_text_links" style="float: left; padding-top: 8px; padding-bottom: 8px;">Home</a>\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl/suggest" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Suggest a Book</a>\n  <a href="https://diversifying-reading.github.io/diversifying-reading-slcpl/resources" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Resources</a>\n  <a href="https://diversifying-reading.github.io/search/" class="topnav_text_links" style="float: right; padding-top: 8px; padding-bottom: 8px;">Catalog</a>\n  ';
-    document.getElementById("menu_button").style.float = "left";
-    document.getElementsByClassName('topnav_text')[0].style.marginLeft = "1vmin";
-
-    scroll_function();
-  }
-  if(window.innerWidth > 687){
-    document.getElementsByClassName("topnav_text")[0].style.fontSize = document.getElementById("topnav").offsetHeight/2 +"px";
-    document.getElementsByClassName("topnav_text")[0].style.fontSize = parseFloat(document.getElementsByClassName("topnav_text")[0].style.fontSize) * document.getElementById("topnav").offsetHeight/document.getElementsByClassName("topnav_text")[0].offsetHeight;
-  }
-
-  if(document.getElementsByClassName("select").length > 0){
-    document.getElementsByClassName("select")[0].style.width = window.innerWidth - (document.getElementById("topnav").offsetHeight/2 + document.getElementById("topnav").offsetHeight*4/7) + "px";
-  }
-
-	document.getElementById("menu_button").style.height = document.getElementById("topnav").offsetHeight-document.getElementById("topnav").offsetHeight/2 +"px";
-  document.getElementById("menu_button").style.paddingLeft = document.getElementById("topnav").offsetHeight/3 +"px";
-  document.getElementById("menu_button").style.paddingRight = document.getElementById("topnav").offsetHeight*2/7 +"px";
-  document.getElementById("menu_button").style.paddingTop = (document.getElementById("topnav").offsetHeight-parseInt(document.getElementById("menu_button").style.height))/2-2.5 +"px";
-  document.getElementById("menu_button").style.paddingBottom = (document.getElementById("topnav").offsetHeight-parseInt(document.getElementById("menu_button").style.height))/2-2.5 +"px";
+  resize_images();
 }
 
 function currentRepository(url_test){
@@ -143,5 +130,4 @@ else if(currentRepository("https://diversifying-reading.github.io/search/?search
 else if(currentRepository("https://diversifying-reading.github.io/search/?search=undefined&page=0") == "search"){
   topnav_text_links_index = 3;
 }
-
-document.getElementsByClassName("topnav_text_links")[topnav_text_links_index].className = "currentTextLink";
+// document.getElementsByClassName("topnav_text_links")[topnav_text_links_index].className = "currentTextLink";
