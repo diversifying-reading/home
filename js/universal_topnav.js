ec97cd51b1f362566460da40a3cc41fc6306d8b7
Rayne Weinstein <98506458+RZW-13@users.noreply.github.com>
2023-04-22 23:48:46 -0500

    Update universal_topnav.js

--- js/universal_topnav.js
+++ js/universal_topnav.js
@@ -2,6 +2,9 @@ var window_url = "https://diversifying-reading.github.io/home/?search=undefined"
 var repository = window_url.split("https://diversifying-reading.github.io/")[1].split("/")[0];
 var scrollOnLoad = document.documentElement.scrollTop;
 var screenWidth = window.innerWidth;
+if(mobileDevice()){
+  screenWidth = screen.width;
+}
 
 for(let i = 0; i < document.getElementsByClassName("topnav_text").length; i++){
   if(document.getElementsByClassName("topnav_text")[i].innerHTML.toUpperCase() == repository.toUpperCase()){
@@ -10,7 +13,6 @@ for(let i = 0; i < document.getElementsByClassName("topnav_text").length; i++){
 }
 
 function mobileDevice() {
-
      if (navigator.userAgent.match(/Android/i)
      || navigator.userAgent.match(/webOS/i)
      || navigator.userAgent.match(/iPhone/i)
@@ -22,21 +24,6 @@ function mobileDevice() {
      } else {
         return false ;
      }
-}
-
-if(mobileDevice()){
-  for(i=0; i<document.getElementsByTagName("p").length; i++){
-    document.getElementsByTagName("p")[i].style.fontSize = "38px"
-  }
-  for(i=0; i<document.getElementsByTagName("h2").length; i++){
-    document.getElementsByTagName("h2")[i].style.fontSize = "54px"
-  }
-  for(i=0; i<document.getElementsByTagName("h1").length; i++){
-    document.getElementsByTagName("h1")[i].style.fontSize = "30px"
-  }
-  for(i=0; i<document.getElementsByTagName("h3").length; i++){
-    document.getElementsByTagName("h3")[i].style.fontSize = "20px"
-  }
 }
 
 function selectTopnavUpdate(){
@@ -66,7 +53,7 @@ function resize_topnav(){
     optionsFormatted += "</option>"
   }
 
-  if(window.innerWidth <= 687 && document.getElementsByClassName("topnav_text")[0].style.height == "" || mobileDevice() && document.getElementsByClassName("topnav_text")[0].style.height == ""){
+  if(screenWidth <= 687 && document.getElementsByClassName("topnav_text")[0].style.height == ""){
     document.getElementsByClassName("topnav_text")[0].innerHTML = '<select class="select" onchange="selectTopnavUpdate()">' + optionsFormatted + '</select>';
     document.getElementsByClassName("topnav_text")[0].style.height = "100%";
     document.getElementsByClassName("topnav_text")[0].style.width = document.getElementsByClassName("select")[0].offsetWidth+10 + "px";
@@ -77,7 +64,7 @@ function resize_topnav(){
 
     scroll_function();
   }
-  else if(window.innerWidth > 687 && document.getElementsByClassName("select").length > 0 && !mobileDevice()){
+  else if(screenWidth > 687 && document.getElementsByClassName("select").length > 0){
     document.getElementsByClassName("topnav_text")[0].style.height = "";
     document.getElementsByClassName("topnav_text")[0].style.paddingLeft = "14px";
     document.getElementsByClassName("topnav_text")[0].style.width = "";
@@ -87,13 +74,41 @@ function resize_topnav(){
 
     scroll_function();
   }
-  if(window.innerWidth > 687 && !mobileDevice()){
+  if(screenWidth > 687){
     document.getElementsByClassName("topnav_text")[0].style.fontSize = document.getElementById("topnav").offsetHeight/2 +"px";
     document.getElementsByClassName("topnav_text")[0].style.fontSize = parseFloat(document.getElementsByClassName("topnav_text")[0].style.fontSize) * document.getElementById("topnav").offsetHeight;
   }
 
   if(document.getElementsByClassName("select").length > 0){
     document.getElementsByClassName("select")[0].style.width = "100vw";
+  }
+
+  if(mobileDevice() && document.getElementsByClassName("select").length > 0){
+    // 387
+    // document.getElementsByClassName('select')[0].style.fontSize = 74 + "px";
+    // document.getElementsByClassName('select')[0].style.paddingLeft = 33 + "px"
+    // document.getElementById("topnav").style.height = parseInt(document.getElementsByClassName("select")[0].style.fontSize) + 51 + "px";
+    // document.getElementById("topnav").style.borderBottom = 14 + "px solid #7fa569"
+    // // 687
+    document.getElementsByClassName('select')[0].style.fontSize = 44 + ((687-screenWidth)/10) + "px";
+    document.getElementsByClassName('select')[0].style.paddingLeft = 15 + ((687-screenWidth)*3/50) + "px"
+    document.getElementById("topnav").style.height = parseInt(document.getElementsByClassName("select")[0].style.fontSize) + 26 + ((687-screenWidth)/12)+ "px";
+    document.getElementById("topnav").style.borderBottom = 7 + ((687-screenWidth)*7/300) + "px solid #7fa569"
+  }
+  else if(mobileDevice() && screenWidth <= 1390){
+    // // 1390
+    document.getElementsByClassName('topnav_text')[0].style.fontSize = 27 + ((1390-screenWidth)*11/700) + "px";
+    document.getElementsByClassName("currentTextLink")[0].style.paddingTop = 8 + ((1390-screenWidth)*3/700) + "px";
+    document.getElementsByClassName("currentTextLink")[0].style.paddingBottom = 8 + ((1390-screenWidth)*3/700) + "px";
+    document.getElementsByClassName("topnav_text_links")[0].style.paddingTop = 8 + ((1390-screenWidth)*3/700) + "px";
+    document.getElementsByClassName("topnav_text_links")[1].style.paddingTop = 8 + ((1390-screenWidth)*3/700) + "px";
+    document.getElementsByClassName("topnav_text_links")[2].style.paddingTop = 8 + ((1390-screenWidth)*3/700) + "px";
+    document.getElementById("topnav").style.height = 49  + ((1390-screenWidth)*2/70)+ "px";
+    document.getElementById("topnav").style.borderBottom = 5  + ((1390-screenWidth)*2/700)+ "px solid #7fa569"
+
+    // document.getElementsByClassName('topnav_text')[0].style.fontSize = 27 + "px";
+    // document.getElementById("topnav").style.height = 49 + "px";
+    // document.getElementById("topnav").style.borderBottom = 5 + "px solid #7fa569"
   }
 }
 
@@ -119,7 +134,7 @@ function scroll_function(){
     topnav_paddingTop = 17.5;
   }
 
-  if(window.innerWidth <= 1390){
+  if(screenWidth <= 1390){
     topnav_paddingTop = 8;
   }
 
